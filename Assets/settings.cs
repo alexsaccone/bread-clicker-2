39562295b02cb011e6cf7c853e8ee20fc3205649
spiceless <saccone.alexander.c@gmail.com>
2025-05-26 17:53:37 -0400

    Added settomgs button that prints stuff

---
+++ Assets/settings.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class settings : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+            if (hit.collider != null && hit.collider.gameObject == gameObject)
+            {
+                Debug.Log("Open settings menu");
+            }
+        }
+    }
+}
